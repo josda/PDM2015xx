15af91c68680641aebd0728396503b1c6ea4c161
josda <josefin.dahlbom@neuro.uu.se>
2015-07-22 13:34:40 +0200

    WEather 6 variables

--- model/WeatherDataAdapter.m
+++ model/WeatherDataAdapter.m
@@ -20,9 +20,9 @@ classdef WeatherDataAdapter < DataAdapter
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         function this = WeatherDataAdapter()
             this.dobj = Observation();
-            %this.cell_ = {'/weatherTime','W_temp','W_humid','W_Wind speed','W_Wind dir','W_Pressure','W_Radiation'};
-            this.cell_ = {'/weatherTime','W_temp','W_humid','W_Wind speed'};
-            this.nrOfNewVariables = 0; %Change this to 3 when using the correct weatherfile
+            this.cell_ = {'/weatherTime','W_temp','W_humid','W_Wind speed','W_Wind dir','W_Pressure','W_Radiation'};
+%             this.cell_ = {'/weatherTime','W_temp','W_humid','W_Wind speed'};
+            this.nrOfNewVariables = 3; %Change this to 3 when using the correct weatherfile
             this.tempMatrix = this.cell_;
         end
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
